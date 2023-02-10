52ab3802f1f2c7c733224afc443720ffcc289a5c
MaykonPG <maykonpgadm@hotmail.com>
2023-02-10 16:30:35 -0300

    carregando projeto inicial

---
+++ src/main/java/com/api/Parking/Control/repositories/PardkingSpotRepository.java
@@ -0,0 +1,11 @@
+package com.api.Parking.Control.repositories;
+
+import com.api.Parking.Control.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PardkingSpotRepository extends JpaRepository <ParkingSpotModel, UUID> {
+}
